ee531b7eae14c78b1a30e33dc0b1f9bbd4e60915
jefflennon1 <jeffersonlennoncastro@gmail.com>
2026-05-14 18:20:48 -0300

    First commit, iniitial project

---
+++ src/main/java/com/noriservices/norisales/NoriSalesApplication.java
@@ -0,0 +1,13 @@
+package com.noriservices.norisales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoriSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoriSalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/noriservices/norisales/NoriSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noriservices.norisales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoriSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
