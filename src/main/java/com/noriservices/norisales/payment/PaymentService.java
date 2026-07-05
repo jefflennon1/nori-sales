6f91a8aebc184e4eebfff2e8417f309895ca5294
jefflennon1 <jeffersonlennoncastro@gmail.com>
2026-07-05 16:02:19 -0300

    Creating dedicated classe for Payment confirmation and publication event

--- src/main/java/com/noriservices/norisales/payment/PaymentService.java
+++ src/main/java/com/noriservices/norisales/payment/PaymentService.java
@@ -17,14 +17,8 @@ import com.noriservices.norisales.shared.exception.PendingPaymentException;
 import com.noriservices.norisales.shared.exception.ResourceNotFoundException;
 import com.noriservices.norisales.user.User;
 import com.noriservices.norisales.user.UserService;
-import com.noriservices.norisales.order.event.DTO.OrderConfirmedEventDTO;
-import com.noriservices.norisales.order.event.DTO.OrderItemEventDTO;
-import com.noriservices.norisales.order.event.OrderEventProducer;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -36,8 +30,8 @@ public class PaymentService {
     private final PaymentMapper mapper;
     private final OrderService orderService;
     private final UserService userService;
-    private final OrderEventProducer saleEventProducer;
     private final MercadoPagoGateway mercadoPagoGateway;
+    private final PaymentConfirmationService paymentConfirmationService;
 
 
     public PaymentResponseDTO generatePaymentByPix(UUID orderId){
@@ -87,7 +81,6 @@ public class PaymentService {
         }
     }
 
-    @Transactional
     public void processPaymentWebhook(WebhookDTO webhookDTO) {
       try {
 
@@ -96,25 +89,9 @@ public class PaymentService {
 
               if(payment.getStatus().equals(PaymentStatus.APPROVED)) return;
 
-              payment.setStatus(PaymentStatus.APPROVED);
-              payment.setPaidAt(LocalDateTime.now());
-              repository.save(payment);
-              orderService.confirmPayment(payment.getOrder().getId());
 
-              Order order = orderService.findById(payment.getOrder().getId());
-              OrderConfirmedEventDTO event = new OrderConfirmedEventDTO(
-                      order.getId(),
-                      order.getUser().getId(),
-                      order.getItems().stream()
-                              .map(item -> new OrderItemEventDTO(
-                                      item.getProduct().getId(),
-                                      item.getQuantity()
-                              ))
-                              .toList(),
-                      LocalDateTime.now()
-              );
+              paymentConfirmationService.confirmApprovedPayment(webhookDTO.data().id());
 
-              saleEventProducer.publishOrderConfirmed(event);
           }
       } catch (PaymentProviderException ex){
           throw new PaymentProviderException("Failed to process payment webhook. ", ex.getCause());
